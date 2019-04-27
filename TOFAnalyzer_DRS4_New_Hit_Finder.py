d27c124952090da99b94ba9b929f7cb08e9a863a
Steven <stevenblock2@gmail.com>
2019-04-27 13:44:12 -0500

    Actually auto-installs dependancies

--- TOFAnalyzer_DRS4_New_Hit_Finder.py
+++ TOFAnalyzer_DRS4_New_Hit_Finder.py
@@ -16,11 +16,12 @@ Future work:
 """
 def install_and_import(package):
     import importlib
+
     try:
         importlib.import_module(package)
     except ImportError:
-        import pip
-        pip.main(['install', package])
+        import subprocess
+        subprocess.call(['pip', 'install', package])
     finally:
         globals()[package] = importlib.import_module(package)
 
